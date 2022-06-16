8813eda3430771418e9b1e9b5ee85bbf5f451e94
Precious Akams <preciousakams@yahoo.com>
2022-06-16 04:02:31 +0100

    render with slice

--- src/components/NewBook.js
+++ src/components/NewBook.js
@@ -1,18 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
-const NewBook = () => (
-  <div className="newBook">
-    <h1>ADD NEW BOOK</h1>
-    <form>
-      <input type="text" placeholder="Book title" />
-      <select>
-        <option>Author</option>
-        <option>Author 1</option>
-        <option>Author 2</option>
-      </select>
-      <button type="button" className="addBookBtn">ADD BOOK</button>
-    </form>
-  </div>
-);
+const NewBook = () => {
+  const [titleValue, setTitleValue] = useState('');
+  const [authorValue, setAuthorValue] = useState('');
+  const dispatch = useDispatch();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (titleValue && authorValue) {
+      dispatch(
+        addBook({
+          title: titleValue,
+          author: authorValue,
+        }),
+      );
+    }
+  };
+  return (
+    <div className="newBook">
+      <h1>ADD NEW BOOK</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Book title"
+          value={titleValue}
+          onChange={(e) => setTitleValue(e.target.value)}
+        />
+        <select name="authors">
+          <option
+            value={authorValue}
+            onChange={(e) => setAuthorValue(e.target.value)}
+          >
+            Author1
+
+          </option>
+          <option
+            value={authorValue}
+            onChange={(e) => setAuthorValue(e.target.value)}
+          >
+            Author2
+
+          </option>
+          <option
+            value={authorValue}
+            onChange={(e) => setAuthorValue(e.target.value)}
+          >
+            Author3
+
+          </option>
+        </select>
+        <button type="button" className="addBookBtn">ADD BOOK</button>
+      </form>
+    </div>
+  );
+};
 
 export default NewBook;
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,35 +1,23 @@
-const ADD_BOOK = 'bookStore/books/ADD_BOOK';
-const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = [];
-
-export const addbook = (title, author) => ({
-  type: ADD_BOOK,
-  bookId: Date.now(),
-  title,
-  author,
+export const bookSlice = createSlice({
+  name: 'books',
+  initialState: [
+    { bookId: 1, title: 'title1', author: 'author1' },
+    { bookId: 2, title: 'title2', author: 'author2' },
+    { bookId: 3, title: 'title3', author: 'author30' },
+  ],
+  reducers: {
+    addBook: (state, action) => {
+      const book = {
+        bookId: new Date(),
+        title: action.payload.title,
+        author: action.payload.action,
+      };
+      state.push(book);
+    },
+  },
 });
 
-export const removeBook = (bookId) => ({
-  type: REMOVE_BOOK,
-  bookId,
-});
-
-/// /// Reducers
-
-const bookReducer = (state = initialState, action) => {
-  switch (action.type) {
-    default:
-      return state;
-    case ADD_BOOK:
-      return [...state, {
-        bookId: action.bookId,
-        title: action.title,
-        author: action.author,
-      }];
-    case REMOVE_BOOK:
-      return state.filter((book) => book.bookId !== action.bookId);
-  }
-};
-
-export default bookReducer;
+export const { addBook } = bookSlice.actions;
+export default bookSlice.reducer;
